cbdc50ab939eae38e38a05fc81b8dcb9c3cb60b7
mm <maksmikhilov01@gmail.com>
2024-10-13 20:27:19 +0500

    dev

--- pkg/api/node.py
+++ pkg/api/node.py
@@ -14,6 +14,7 @@ class NodeClient(model.INodeClient):
             async with session.get(f"http://{node_ip}:{self.node_metric_port}{path}") as resp:
                 return await resp.json()
 
+
     async def __async_delete(self, path: str, node_ip: str):
         async with aiohttp.ClientSession() as session:
             async with session.delete(f"http://{node_ip}:{7000}{path}") as resp:
